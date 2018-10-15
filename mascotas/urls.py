a8f43b180ab5c8d45ae71e3edc085c63e0decc30
sleivav <sergio.leiva@ing.uchile.cl>
2018-10-15 16:05:18 -0300

    Agregar path para subir fotos a urls.py

--- mascotas/urls.py
+++ mascotas/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name=''),
-    path('<int:humano>', views.humano, name='humano')
+    path('<int:humano>', views.humano, name='humano'),
+    path('/subir_foto/<int: animal>', views.subir_foto, name='subir_foto'),
 ]
